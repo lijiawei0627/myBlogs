eb6f768fdf5cc374e468e493a7aef525ad5aee68
ljw <1472406718@qq.com>
2020-03-21 12:02:18 +0800

    用户登录路由完成

--- blog1/src/router/user.js
+++ blog1/src/router/user.js
@@ -1,11 +1,16 @@
-const handleUserRouter = (req, res) => {
-  const method = req.methodl
+const { loginCheck } = require('../controller/user')
+const { SuccessModel, ErrorModel } = require('../model/resModel')
 
+const handleUserRouter = (req, res) => {
+  const method = req.method
   // 登录
   if (method === 'POST' && req.path === '/api/user/login') {
-    return {
-      msg: '这是登录接口'
+    const { username, password } = req.body
+    const result = loginCheck(username, password)
+    if (result) {
+      return new SuccessModel()
     }
+    return new ErrorModel('登录失败')
   }
   // 注册
   if (method === 'POST' && req.path === '/api/user/register') {
