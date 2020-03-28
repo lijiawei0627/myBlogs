c79bbc714a0090c03583078c3de7b498ad859de9
ljw <1472406718@qq.com>
2020-03-28 11:04:46 +0800

    博客路由完成

--- blog-express/routes/blog.js
+++ blog-express/routes/blog.js
@@ -1,9 +1,10 @@
 const { getList, getDetail, newBlog, updateBlog, delBlog } = require('../controller/blog')
 const { SuccessModel, ErrorModel } = require('../model/resModel')
+const loginCheck = require('../middleware/loginCheck')
 var express = require('express');
 var router = express.Router();
 
-router.get('/list', function(req, res, next) {
+router.get('/list', loginCheck, (req, res, next) => {
   // express中的req自带了query，不用再去手动解析query
   const author = req.query.author || ''
   const keyword = req.query.keyword || ''
@@ -13,5 +14,47 @@ router.get('/list', function(req, res, next) {
     res.json(new SuccessModel(listData))
   })
 });
+
+// 获取博客详情
+router.get('/detail', (req, res, next) => {
+  // 从数据库获取数据，注意返回的result为一个promise
+  const result = getDetail(req.query.id)
+  result.then(data => {
+    res.json(new SuccessModel(data))
+  })
+})
+
+// 新建一篇博客
+router.post('/new', loginCheck, (req, res, next) => {
+  req.body.author = req.session.username
+  const result = newBlog(req.body)
+  result.then(data => {
+    res.json(new SuccessModel(data))
+  })
+})
+  
+// 更新一篇博客
+router.post('/update', loginCheck, (req, res, next) => {
+  const result = updateBlog(req.query.id, req.body)
+  result.then(val => {
+    if (val) {
+      res.json(new SuccessModel())
+    } else {
+      res.json(new ErrorModel('更新博客失败'))
+    }
+  })
+})
+// 删除一篇博客
+router.post('/delete', loginCheck, (req, res, next) => {
+  const author = req.session.username
+  const result = delBlog(req.query.id, author)
+  result.then(val => {
+    if (val) {
+      res.json(new SuccessModel())
+    } else {
+      res.json(new ErrorModel('删除博客失败'))
+    }
+  })
+})
 
 module.exports = router;
