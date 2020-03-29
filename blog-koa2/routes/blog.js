006a25a58932b5ed741b62352ea786ac29e3ffc3
ljw <1472406718@qq.com>
2020-03-29 11:58:46 +0800

    路由接口开发完成

--- blog-koa2/routes/blog.js
+++ blog-koa2/routes/blog.js
@@ -1,3 +1,6 @@
+const { getList, getDetail, newBlog, updateBlog, delBlog } = require('../controller/blog')
+const { SuccessModel, ErrorModel } = require('../model/resModel')
+const loginCheck = require('../middleware/loginCheck')
 const router = require('koa-router')()
 
 // 路由前缀
@@ -6,11 +9,44 @@ router.prefix('/api/blog')
 // / ctx为req、res的集合体，next与express差不多
 //   /api/blog/list
 router.get('/list', async function (ctx, next) {
-  const query = ctx.query
-  ctx.body = {
-    error: 0,
-    query,
-    data: '获取博客列表'
+  const author = ctx.query.author || ''
+  const keyword = ctx.query.keyword || ''
+  // 从数据库获取数据，注意getList返回的是一个promise
+  const listData = await getList(author, keyword)
+  ctx.body = new SuccessModel(listData)
+})
+
+// 获取博客详情
+router.get('/detail', async (ctx, next) => {
+  // 从数据库获取数据，注意getDetail返回的result为一个promise
+  const data = await getDetail(req.query.id)
+  ctx.body = new SuccessModel(data)
+})
+
+router.post('/new', loginCheck, async (req, res, next) => {
+  const body = ctx.reqest.body
+  body.author = ctx.session.username
+  const data = await newBlog(body)
+  ctx.body = new SuccessModel(data)
+})
+  
+// 更新一篇博客
+router.post('/update', loginCheck, (req, res, next) => {
+  const val = await updateBlog(ctx.query.id, ctx.request.body)
+  if (val) {
+    ctx.body = new SuccessModel()
+  } else {
+    ctx.body = new ErrorModel('更新博客失败')
+  }
+})
+// 删除一篇博客
+router.post('/delete', loginCheck, (req, res, next) => {
+  const author = ctx.session.username
+  const val = await delBlog(req.query.id, author)
+  if (val) {
+    ctx.body = new SuccessModel()
+  } else {
+    ctx.body = new ErrorModel('删除博客失败')
   }
 })
 
--- blog-koa2/routes/user.js
+++ blog-koa2/routes/user.js
@@ -1,3 +1,5 @@
+const { login } = require('../controller/user')
+const { SuccessModel, ErrorModel } = require('../model/resModel')
 const router = require('koa-router')()
 
 // 路由前缀
@@ -5,24 +7,30 @@ router.prefix('/api/user')
 
 router.post('/login', async function (ctx, next) {
   const { username, password } = ctx.request.body
-  ctx.body = {
-    error: 0,
-    username,
-    password
+  const data = await login(username, password)
+  if (data.username) {
+
+    // 设置session
+    ctx.session.username = data.username
+    ctx.session.realname = data.realname
+
+    ctx.body = new SuccessModel(data)
+    return
   }
+  ctx.body = new ErrorModel('登录失败')
 })
 
-router.get('/session-test', async function (ctx, next) {
-  console.log(1)
-  if (ctx.session.viewCount == null) {
-    ctx.session.viewCount = 0
-  }
-  ctx.session.viewCount++
+// router.get('/session-test', async function (ctx, next) {
+//   console.log(1)
+//   if (ctx.session.viewCount == null) {
+//     ctx.session.viewCount = 0
+//   }
+//   ctx.session.viewCount++
 
-  ctx.body = {
-    error: 0,
-    viewCount: ctx.session.viewCount
-  }
-})
+//   ctx.body = {
+//     error: 0,
+//     viewCount: ctx.session.viewCount
+//   }
+// })
 
 module.exports = router
